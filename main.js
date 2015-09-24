fdf2e2ccb40623b1835d7d1128eeb40bf95dc6ff
Anna Roberts <annaroberts960608@gmail.com>
2015-09-24 12:19:29 -0400

    add president objects

--- main.js
+++ main.js
@@ -3,3 +3,50 @@ var iceCream = [
 	"strawberry"
 	"sea salt and carmel"
 ]
+
+
+
+var BarrackObama = {
+	firstName = 'Barrack'
+	lastName = 'Obama'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency  = '2009-present day'
+}
+
+var GeorgeWBush = {
+
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency  = '2001-2009'
+}
+
+var BillClinton = {
+
+	firstName = 'Bill'
+	lastName = 'Clinton'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency  = '1993-2001'
+}
+
+var GeorgeBush = {
+
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'one term'
+	party = 'Republican'
+	yearsOfPresidency  = '1989-1993'
+}
+
+var RonaldReagan = {
+
+	firstName = 'Ronald'
+	lastName = 'Reagan'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency  = '1981-1989'
+}
+
