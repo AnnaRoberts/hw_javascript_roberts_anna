711db204fdc5c2ad76104ffd8da483136296fcfb
Anna Roberts <annaroberts960608@gmail.com>
2015-09-25 14:45:42 -0400

    add a fourth string on the icecream array

--- main.js
+++ main.js
@@ -1,5 +1,100 @@
+
+//Array//
+
 var iceCream = [
 	"chocolate chip cookie dough"
 	"strawberry"
 	"sea salt and carmel"
+	"iceCream.push (chocolate mint)"
 ]
+
+
+//Objects//
+
+var presidents = [ 
+
+var BarrackObama = {
+	firstName = 'Barrack'
+	lastName = 'Obama'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency  = '2009-present day'
+}
+
+var GeorgeWBush = {
+
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency  = '2001-2009'
+}
+
+var BillClinton = {
+
+	firstName = 'Bill'
+	lastName = 'Clinton'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency  = '1993-2001'
+}
+
+var GeorgeBush = {
+
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'one term'
+	party = 'Republican'
+	yearsOfPresidency  = '1989-1993'
+}
+
+var RonaldReagan = {
+
+	firstName = 'Ronald'
+	lastName = 'Reagan'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency  = '1981-1989'
+}
+
+]
+
+function myFunction () {
+	console.log (presidents [2], lastName,firstName);
+}
+
+//functions//
+
+
+//function expression 
+
+function howdy () {
+	console.log ('Anna Roberts says hello!')
+}
+
+
+
+//function declararion
+var numba = function ("string") {
+	if (string < numba) {
+		console.log ('What a short little word!');
+
+	else if (string > numba) {
+		console.log ('I am sorry, but that is too many to count');
+	}
+	else if (string = numba) {
+		console.log ('What a perfect choice!');
+	}
+
+function numba ("string") {
+	printToConsole ("7")
+	printToConsole ("seventy")
+	printToConsole ("university")
+}
+
+var inception = function inception ("callback, value") {
+	callback (value);
+	console.log ("Batman is a great movie!")
+};
+	
+
